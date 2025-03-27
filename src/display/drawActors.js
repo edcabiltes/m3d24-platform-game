45e59df031f6944f4994b6d91413223aa7c91384
Ed Cabiltes <edcabiltes@outlook.com>
2025-03-27 11:28:17 +0800

    draw game actors

---
+++ src/display/drawActors.js
@@ -0,0 +1,14 @@
+import { scale } from "../utils/constants.js";
+import { elt } from "../utils/helpers.js";
+
+export function drawActors(actors) {
+  return elt("div", {}, ...actors.map(actor => {
+    let rect = elt("div", {class: `actor ${actor.type}`});
+    rect.style.width = `${actor.size.x * scale}px`;
+    rect.style.height = `${actor.size.y * scale}px`;
+    rect.style.left = `${actor.pos.x * scale}px`;
+    rect.style.top = `${actor.pos.y * scale}px`;
+    return rect;
+  }));
+}
+    
